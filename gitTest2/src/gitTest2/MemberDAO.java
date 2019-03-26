07ae62f9b206cabee886a4ba336d978d772472e5
yong <qkrghdys@naver.com>
2019-03-26 17:10:16 +0900

    memberdao 생성

---
+++ gitTest2/src/gitTest2/MemberDAO.java
@@ -0,0 +1,18 @@
+package gitTest2;
+
+import java.sql.Connection;
+
+public class MemberDAO {
+   private Connection con;
+   
+   public MemberDAO(Connection con) {
+      super();
+      this.con=con;
+   }
+   public void insert() {
+      
+   }
+   public void modify() {
+      
+   }
+}
